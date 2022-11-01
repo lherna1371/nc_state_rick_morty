fe6d09ad266e279f289a19909bd944b37cb99f15
lherna1371 <lherna1371@gmail.com>
2022-10-31 20:01:09 -0500

    updated for review

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { getCharacter } from 'rickmortyapi'
 
-const mainChar = await getCharacter(1);
+// const mainChar = await getCharacter(1);
 let aliveCounter = 0;
 let deadCounter = 0;
 
@@ -12,20 +12,27 @@ document.querySelector("button").addEventListener("click", function(){
 async function callAPI() {
     // 826 characters - get random
     let number = Math.ceil(Math.random()*826);
-    const character = await getCharacter(number);
+    const character = await getCharacter(number); // returns JSON
 
-    removeCurrentImage()
+    // updated the image
+    let image = document.querySelector("#character-image")
+    image.src = character.data.image;
+    image.alt = character.data.name;
 
-    // call a function to figure out if the character is dead/alive and increment counter on page
+    // display character name and status
+    document.querySelector(".character-status").textContent = `${character.data.name} is ${character.data.status}`;
 
-    let image = document.createElement("img");
-    image.setAttribute("src", character.data.image);
-    image.setAttribute("alt", character.data.name);
-    image.setAttribute("id", "character-image")
-    document.querySelector(".character-profile").append(image);
-}
+    character.data.status // contains dead/alive/unknown
 
+    if  (character.data.status === "Alive") {
+        aliveCounter++
+    } else if (character.data.status === "Dead"){
+        deadCounter++
+    } else {
+        console.log("Character status is Unknown!")
+    }
 
-function removeCurrentImage() {
-    document.querySelector("#character-image").remove();
+    document.querySelector("#dead-counter").textContent = deadCounter;
+    document.querySelector("#alive-counter").textContent = aliveCounter;
+
 }
